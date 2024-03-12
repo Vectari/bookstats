016a9983ad1936df64c4097bce68e36ded281eea
Vectari <mateusz.majer996@gmail.com>
2024-03-13 00:59:48 +0100

    first integration to Open Library API

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,5 +1,43 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <h1>Test</h1>;
+  const [data, setData] = useState([]);
+  const [img, setImg] = useState([]);
+  // const backend = "http://localhost:3000";
+
+  useEffect(() => {
+    fetch(`https://openlibrary.org/search.json?title=the+lord+of+the+rings`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error("Loading data error!");
+      })
+      .then((res) => {
+        setData(res);
+        setImg(res.docs[0].author_key);
+      });
+  }, []);
+
+  console.log(data)
+
+  const photo = `https://covers.openlibrary.org/a/olid/${img}.jpg`;
+
+  return (
+    <>
+      <h1>Test</h1>
+      <img src={photo} alt="" />
+      {/* <ul>
+        {data.map(({ id, word, translation }) => (
+          <>
+            <li>{id}</li>
+            <li>{word}</li>
+            <li>{translation}</li>
+          </>
+        ))}
+      </ul> */}
+    </>
+  );
 }
 
 export default App;
