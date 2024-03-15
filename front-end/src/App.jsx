fa6d7e6e4b4e4d2640821e9eb1c5d3afe90cbac0
Vectari <mateusz.majer996@gmail.com>
2024-03-15 23:16:29 +0100

    Add working search bar

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,7 +3,26 @@ import { useState, useEffect } from "react";
 function App() {
   const [data, setData] = useState([]);
   const [img, setImg] = useState([]);
+  const [search, setSearch] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
   // const backend = "http://localhost:3000";
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        `https://openlibrary.org/search.json?title=${search}`
+      );
+      const searchData = await response.json();
+      setSearchResults(searchData.docs); // Update to use `docs` instead of `results`
+    } catch (error) {
+      console.error("Error searching:", error);
+    }
+  };
 
   useEffect(() => {
     fetch(`https://openlibrary.org/search.json?title=the+lord+of+the+rings`)
@@ -22,14 +41,41 @@ function App() {
   //
   //
 
-  console.log(data)
+  console.log(data);
+  console.log(searchResults);
 
-  const photo = `https://covers.openlibrary.org/a/olid/${img}.jpg`;
+  const photo = `https://covers.openlibrary.org/a/olid/${img}-S.jpg`;
 
   return (
     <>
       <h1>Test</h1>
       <img src={photo} alt="" />
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          placeholder="Search title..."
+          value={search}
+          onChange={handleChange}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSearch(e);
+            }
+          }}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <ul>
+        {searchResults.map((result, key) => (
+          <>
+            <li key={key}>
+              Title: {result.title} Author: {result.author_name}
+            </li>
+            <img
+              src={`https://covers.openlibrary.org/b/olid/${result.cover_edition_key}-M.jpg `}
+            />
+          </>
+        ))}
+      </ul>
       {/* <ul>
         {data.map(({ id, word, translation }) => (
           <>
