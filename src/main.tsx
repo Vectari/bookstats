94e82f8688251110811e6b5cf97885f8a4be2a09
Vectari <mateusz.majer996@gmail.com>
2024-07-23 00:12:21 +0200

    feat: add new components

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { SearchBar } from './components/SearchBar/SearchBar'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+import { NavBar } from "./sections/NavBar/NavBar";
+import { Hero } from "./sections/Hero/Hero";
+import { Footer } from "./sections/Footer/Footer";
 
-const search = "https://openlibrary.org/search.json?q=the+lord+of+the+rings"
+const search = "https://openlibrary.org/search.json?q=the+lord+of+the+rings";
 
-console.log(search)
+console.log(search);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hello !!!</h1>
+    <NavBar />
+    <Hero />
     <SearchBar />
-  </React.StrictMode>,
-)
+    <Footer />
+  </React.StrictMode>
+);
