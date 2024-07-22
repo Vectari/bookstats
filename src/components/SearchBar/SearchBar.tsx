2cb02b09e61866455ec123af3d3e3acf080d5851
Vectari <mateusz.majer996@gmail.com>
2024-07-23 00:11:48 +0200

    fix: fix all ts error

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,51 +1,32 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 interface SearchResult {
   edition_key: string;
   title: string;
-  author_name: string;
+  author_name: string[];
   cover_edition_key: string;
 }
 
 export function SearchBar() {
-  const [data, setData] = useState([]);
   const [search, setSearch] = useState("");
-  const [searchResults, setSearchResults] = useState<SearchResult>({
-    edition_key: "",
-    title: "",
-    author_name: "",
-    cover_edition_key: "",
-  });
+  const [searchResults, setSearchResults] = useState<SearchResult[]>([]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
-  const handleSearch = async (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSearch = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
       const response = await fetch(
         `https://openlibrary.org/search.json?title=${search}`
       );
       const searchData = await response.json();
-      setSearchResults(searchData.docs); // Update to use `docs` instead of `results`
+      setSearchResults(searchData.docs);
     } catch (error) {
       console.error("Error searching:", error);
     }
   };
-
-  useEffect(() => {
-    fetch(`https://openlibrary.org/search.json?title=the+lord+of+the+rings`)
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        }
-        throw new Error("Loading data error!");
-      })
-      .then((res) => {
-        setData(res);
-      });
-  }, []);
 
   return (
     <>
@@ -55,16 +36,11 @@ export function SearchBar() {
           placeholder="Search title..."
           value={search}
           onChange={handleChange}
-          onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
-            if (e.key === "Enter") {
-              handleSearch(e);
-            }
-          }}
         />
         <button type="submit">Search</button>
       </form>
       <ol>
-        {searchResults.map((result: SearchResult) => (
+        {searchResults.map((result) => (
           <li key={result.edition_key}>
             <div>Title: {result.title} </div>
             <div>Author: {result.author_name}</div>
@@ -74,15 +50,6 @@ export function SearchBar() {
           </li>
         ))}
       </ol>
-      {/* <ul>
-        {data.map(({ id, word, translation }) => (
-          <>
-            <li>{id}</li>
-            <li>{word}</li>
-            <li>{translation}</li>
-          </>
-        ))}
-      </ul> */}
     </>
   );
 }
