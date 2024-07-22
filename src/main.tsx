ce4816412275fee9fdbf23e2f4dc5da6ecc4c0e5
Vectari <mateusz.majer996@gmail.com>
2024-07-22 23:00:50 +0200

    feat: add SearchBar

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { SearchBar } from './components/SearchBar/SearchBar'
+
+const search = "https://openlibrary.org/search.json?q=the+lord+of+the+rings"
+
+console.log(search)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <h1>Hello !!!</h1>
+    <SearchBar />
   </React.StrictMode>,
 )
