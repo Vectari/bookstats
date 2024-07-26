5a9c1681d11a6680b35889398a82bb61b8e8d43d
Vectari <mateusz.majer996@gmail.com>
2024-07-26 16:50:23 +0200

    feat: add react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+// import { SearchBar } from "./components/SearchBar/SearchBar";
+// import { NavBar } from "./sections/NavBar/NavBar";
+// import { Hero } from "./sections/Hero/Hero";
+// import { Footer } from "./sections/Footer/Footer";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { LogIn } from "./view/LogIn/LogIn";
+import { SignUp } from "./view/SignUp/SignUp";
+import App from "./App";
 import { SearchBar } from "./components/SearchBar/SearchBar";
-import { NavBar } from "./sections/NavBar/NavBar";
-import { Hero } from "./sections/Hero/Hero";
-import { Footer } from "./sections/Footer/Footer";
 
-const search = "https://openlibrary.org/search.json?q=the+lord+of+the+rings";
+// const search = "https://openlibrary.org/search.json?q=the+lord+of+the+rings";
 
-console.log(search);
+// console.log(search);
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     {/* <NavBar />
+//     <Hero />
+//     <SearchBar />
+//     <Footer /> */}
+
+//     {/* <NavBar />
+//     <Outlet /> */}
+//   </React.StrictMode>
+// );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: (
+          <>
+            <SearchBar />
+          </>
+        ),
+      },
+      {
+        path: "/login",
+        element: <LogIn />,
+      },
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NavBar />
-    <Hero />
-    <SearchBar />
-    <Footer />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>
 );
