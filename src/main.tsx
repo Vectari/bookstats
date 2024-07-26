98e6b4d64d004c1b31e521719220498ee4224d46
Vectari <mateusz.majer996@gmail.com>
2024-07-26 17:01:17 +0200

    chore: delete useless code

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import { SearchBar } from "./components/SearchBar/SearchBar";
-// import { NavBar } from "./sections/NavBar/NavBar";
-// import { Hero } from "./sections/Hero/Hero";
-// import { Footer } from "./sections/Footer/Footer";
+import { Hero } from "./sections/Hero/Hero";
+import { Footer } from "./sections/Footer/Footer";
 import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { LogIn } from "./view/LogIn/LogIn";
 import { SignUp } from "./view/SignUp/SignUp";
 import { SearchBar } from "./components/SearchBar/SearchBar";
 import { NavBar } from "./sections/NavBar/NavBar";
-
-// const search = "https://openlibrary.org/search.json?q=the+lord+of+the+rings";
-
-// console.log(search);
-
-// ReactDOM.createRoot(document.getElementById("root")!).render(
-//   <React.StrictMode>
-//     {/* <NavBar />
-//     <Hero />
-//     <SearchBar />
-//     <Footer /> */}
-
-//     {/* <NavBar />
-//     <Outlet /> */}
-//   </React.StrictMode>
-// );
 
 const router = createBrowserRouter([
   {
@@ -33,7 +15,9 @@ const router = createBrowserRouter([
     element: (
       <>
         <NavBar />
+        <Hero />
         <Outlet />
+        <Footer />
       </>
     ),
     children: [
