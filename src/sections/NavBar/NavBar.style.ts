1722216d9632e83f672f512c37a9c9cfc1473d5a
Vectari <mateusz.majer996@gmail.com>
2024-07-28 00:41:38 +0200

    feat: add icon to navbar

--- src/sections/NavBar/NavBar.style.ts
+++ src/sections/NavBar/NavBar.style.ts
@@ -1,8 +1,44 @@
 import styled from "styled-components";
+import { Theme } from "../../Theme";
 
 export const StyledNavbarWrapper = styled.div`
-  background-color: grey;
+  background-color: ${Theme.colors.dark_grey};
   width: 100%;
-  display: flex;
-  justify-content: center;
+  height: 3rem;
+  line-height: 3rem;
+  position: fixed;
+  bottom: 0;
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+    margin: 0;
+    text-align: center;
+  }
+
+  li {
+    display: inline;
+  }
+
+  a {
+    padding: 0.5rem 2rem;
+    border-radius: 2rem;
+    text-decoration: none;
+    color: ${Theme.colors.white};
+    font-size: 1.3rem;
+  }
+
+  .active {
+    background-color: ${Theme.colors.active};
+  }
+
+  @media screen and (max-width: 440px) {
+    a {
+      padding: 0.5rem 1.7rem;
+    }
+
+    .active {
+      background-color: pink;
+    }
+  }
 `;
--- src/sections/NavBar/NavBar.tsx
+++ src/sections/NavBar/NavBar.tsx
@@ -1,6 +1,10 @@
-import { NavLink, useLocation } from "react-router-dom";
-import { StyledNavbarWrapper } from "./NavBar.style";
 import { useEffect, useState } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineUserAdd } from "react-icons/ai";
+import { StyledNavbarWrapper } from "./NavBar.style";
 
 export function NavBar() {
   const location = useLocation();
@@ -12,41 +16,42 @@ export function NavBar() {
 
   return (
     <>
-      <StyledNavbarWrapper>Navbar</StyledNavbarWrapper>
-      <ul>
-        <li>
-          <NavLink
-            to="/"
-            className={activeLink === ("/" || "/main") ? "active" : ""}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/search"
-            className={activeLink === "/search" ? "active" : ""}
-          >
-            Search
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/login"
-            className={activeLink === "/login" ? "active" : ""}
-          >
-            Log in
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/signup"
-            className={activeLink === "/signup" ? "active" : ""}
-          >
-            Sign up
-          </NavLink>
-        </li>
-      </ul>
+      <StyledNavbarWrapper>
+        <ul>
+          <li>
+            <NavLink
+              to="/"
+              className={activeLink === ("/" || "/main") ? "active" : ""}
+            >
+              <AiOutlineHome />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/search"
+              className={activeLink === "/search" ? "active" : ""}
+            >
+              <AiOutlineSearch />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/login"
+              className={activeLink === "/login" ? "active" : ""}
+            >
+              <AiOutlineUser />
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/signup"
+              className={activeLink === "/signup" ? "active" : ""}
+            >
+              <AiOutlineUserAdd />
+            </NavLink>
+          </li>
+        </ul>
+      </StyledNavbarWrapper>
     </>
   );
 }
