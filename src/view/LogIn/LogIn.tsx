d07550e51eec0efc1ad6bdbdd9cd4a52e350a502
Vectari <mateusz.majer996@gmail.com>
2024-07-27 18:09:18 +0200

    feat: add content to login and signup

--- src/view/LogIn/LogIn.tsx
+++ src/view/LogIn/LogIn.tsx
@@ -1,7 +1,16 @@
 export function LogIn() {
   return (
     <>
-      <h1>LogIn</h1>
+      <h1>Log In</h1>
+      <form action="">
+        <label htmlFor="">Username: </label>
+        <input type="text" placeholder="Enter Username" />
+        <br />
+        <label htmlFor="">Password: </label>
+        <input type="text" placeholder="Enter Password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 }
