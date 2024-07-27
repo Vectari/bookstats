acf94b116377646e1ee0a483f406c21d6a70b3ba
Vectari <mateusz.majer996@gmail.com>
2024-07-28 00:40:41 +0200

    chore: change loader code

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,8 +1,10 @@
+import { StyledLoader } from "./Loader.style";
+
 export function Loader() {
   return (
-    <>
+    <StyledLoader>
       <h1>Loading, please wait...</h1>
-    </>
+    </StyledLoader>
   );
 }
 
