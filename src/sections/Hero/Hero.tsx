6b87fe8aa1ae3fca258ff419dea7c969f97d9e49
Vectari <mateusz.majer996@gmail.com>
2024-07-30 21:11:35 +0200

    style: add some style to hero

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,9 +1,19 @@
 import { RandomQuote } from "../../components/RandomQuote/RandomQuote";
+import { StyledHeroWrapper } from "./Hero.style";
 
 export function Hero() {
   return (
     <>
-      <RandomQuote />
+      <StyledHeroWrapper>
+        <RandomQuote />
+      </StyledHeroWrapper>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. A nobis
+        provident corrupti! Omnis ipsam harum expedita illo adipisci excepturi
+        facere tenetur laborum? Cupiditate molestias id alias eos earum suscipit
+        quaerat quis odit dolorem illum nam, ea mollitia nisi excepturi porro,
+        saepe culpa ipsum, neque animi repellat! Necessitatibus ea rem neque!
+      </h1>
     </>
   );
 }
