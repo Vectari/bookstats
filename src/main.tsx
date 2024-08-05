5b2ca2d46acf111a4a3c6c80a570ca6c32f305f1
Vectari <mateusz.majer996@gmail.com>
2024-08-05 23:06:26 +0200

    chore: add content from App.tsx to main.tsx, delete App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 // import { Footer } from "./sections/Footer/Footer";
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import { BrowserRouter as Router, createBrowserRouter, Outlet, Route, RouterProvider, Routes } from "react-router-dom";
 import { LogIn } from "./view/LogIn/LogIn";
 import { SignUp } from "./view/SignUp/SignUp";
 import { NavBar } from "./sections/NavBar/NavBar";
 import { Search } from "./view/Search/Search";
 import { Home } from "./view/Home/Home";
 import { BookDetail } from "./view/BookDetail/BookDetail";
+import { SearchBar } from "./components/SearchBar/SearchBar";
+
+export function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<SearchBar />} />
+        <Route path="/detail/:edition_key" element={<BookDetail />} />
+      </Routes>
+    </Router>
+  );
+}
 
 const router = createBrowserRouter([
   {
